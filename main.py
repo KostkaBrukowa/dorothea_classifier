09b3bf32b86f9b21135f89a6b95d30df544904af
kostkabrukowa <jiirra@gmail.com>
2020-06-01 11:29:16 +0200

    Plot images for combined scores

--- main.py
+++ main.py
@@ -19,37 +19,41 @@ def read_values(path):
         return results
 
 
-def plot_values(data, title, ylabel):
+def plot_values(data, title, lineLabel, ylabel):
     x1 = [x[0] for x in data]
     y1 = [x[1] for x in data]
 
-    plt.plot(x1, y1, 'o-')
+    plt.plot(x1, y1, label=lineLabel)
     plt.title(title)
     plt.xlabel('Generation number')
     plt.ylabel(ylabel)
     plt.xlim((0, 100))
-    plt.ylim((0.3, 1.1))
-    # plt.ylim((1, 1200)) for mean attributes
+    if y1[0] >= 1:
+        plt.ylim((1, 1200))  # for mean attributes
+    else:
+        plt.ylim((0.3, 1.1)) # for rest
 
 
-
-def plot_type(selection, population):
+def plot_type_best(selection, population):
     file_name_prefix = f"Population {population}/{selection}_Fitness_precision"
     best_individuals = read_best_individuals(file_name_prefix)
+
+    plot_values(best_individuals, 'Best individual', f'Population {population} {selection}', 'Best individual score')
+
+
+def plot_type_mean(selection, population):
+    file_name_prefix = f"Population {population}/{selection}_Fitness_precision"
     mean = read_mean(file_name_prefix)
+
+    plot_values(mean, 'Population mean score', f'Population {population} {selection}', 'Population mean')
+
+
+def plot_type_attrs(selection, population):
+    file_name_prefix = f"Population {population}/{selection}_Fitness_precision"
     mean_attributes = read_mean_attributes(file_name_prefix)
 
-    plot_values(best_individuals, f'Best individual score by generation.\n {file_name_prefix}', 'Best individual score')
-    plt.savefig(f"best_images/Population{population}/{selection}")
-    plt.show()
-
-    plot_values(mean, f'Population mean score by generation.\n {file_name_prefix}', 'Population mean')
-    plt.savefig(f"mean_images/Population{population}/{selection}")
-    plt.show()
-
-    plot_values(mean_attributes, f'Mean attributes count by generation.\n {file_name_prefix}', 'Mean attributes count')
-    plt.savefig(f"attrs_images/Population{population}/{selection}")
-    plt.show()
+    plot_values(mean_attributes, 'Population mean attributes count', f'Population {population} {selection}',
+                'Mean attributes count')
 
 
 def read_best_individuals(path):
@@ -71,4 +75,22 @@ if __name__ == '__main__':
     # Reading
     for population in [100, 700]:
         for selection in [SelectionAlgorithm.Tournament, SelectionAlgorithm.Roulette, SelectionAlgorithm.Ranking]:
-            plot_type(selection, population)
+            plot_type_best(selection, population)
+
+    plt.legend()
+    plt.savefig(f"combined/best")
+    plt.show()
+    for population in [100, 700]:
+        for selection in [SelectionAlgorithm.Tournament, SelectionAlgorithm.Roulette, SelectionAlgorithm.Ranking]:
+            plot_type_mean(selection, population)
+
+    plt.legend()
+    plt.savefig(f"combined/mean")
+    plt.show()
+
+    for population in [100, 700]:
+        for selection in [SelectionAlgorithm.Tournament, SelectionAlgorithm.Roulette, SelectionAlgorithm.Ranking]:
+            plot_type_attrs(selection, population)
+    plt.legend()
+    plt.savefig(f"combined/mean_attrs")
+    plt.show()
